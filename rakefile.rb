c612450ce470b745d20b05d8feb334d758cf2ad9
Sebastian Leal <sebastianleaj10@gmail.com>
2020-03-23 12:56:34 -0500

    asdasd

--- rakefile.rb
+++ rakefile.rb
@@ -2,7 +2,7 @@ require 'albacore'
 
 task :default => [:msbuild]
 
-msbuild :msbuild do |msb|
+build :msbuild do |msb|
   msb.properties = { :configuration => :Debug }
   msb.targets = [ :Clean, :Build ]      
   msb.solution = "AlbacoreDemo.sln"
