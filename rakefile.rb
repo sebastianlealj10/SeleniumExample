e4323e67e6327b13a5436f8937dc4b44e587b51c
Sebastian Leal <sebastianleaj10@gmail.com>
2020-03-23 17:09:54 -0500

    sdasdsa

--- rakefile.rb
+++ rakefile.rb
@@ -1,15 +1,9 @@
 require 'albacore'
 
-task :default => [:msbuild, :mstest]
+task :default => [:msbuild]
 
 build :msbuild do |msb|
   msb.file = 'SeleniumExample.sln'
   msb.properties = { :configuration => :Debug }
   msb.target = [:Restore, :Clean, :Build ]      
-end
-
-desc "Run all nunit tests"
-mstest :mstest=> [:msbuild] do |nunit|
-    nunit.command = "nunit-console.exe"
-    nunit.assemblies "bin\Debug\SeleniumExample.dll"
 end
