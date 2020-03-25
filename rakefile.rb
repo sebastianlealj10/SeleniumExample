93fe3eb97a7b233d1524c7157999c2cf1c35d103
Sebastian Leal <sebastianleaj10@gmail.com>
2020-03-25 16:42:18 -0500

    sadasd

--- rakefile.rb
+++ rakefile.rb
@@ -12,5 +12,4 @@ desc "Run all nunit tests"
 nunit :test => [:msbuild] do |nunit|
   nunit.command = "C:\TeamCity\buildAgent\tools\NUnit.Console.3.11.1\bin\net35\nunit3-console.exe"
   nunit.assemblies = FileList["bin\Debug\SeleniumExample.dll"]
-  nunit.no_logo
 end
