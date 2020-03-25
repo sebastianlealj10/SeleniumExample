6a8c896a8020331753eb99b8e57b0f364da241ec
Sebastian Leal <sebastianleaj10@gmail.com>
2020-03-25 18:09:44 -0500

    asdfasdf

--- rakefile.rb
+++ rakefile.rb
@@ -8,8 +8,7 @@ msbuild :build do |cmd|
   cmd.properties = { :Configuration => :Debug }
 end
 
-desc "Run all nunit tests"
 nunit :test do |nunit| 
   nunit.command = "C:\TeamCity\buildAgent\tools\NUnit.Console.3.11.1\bin\net20\nunit3-console.exe"
-  nunit.assemblies = "C:\TeamCity\buildAgent\work\884196217c13b442\bin\Debug\SeleniumExample.dll"
+  nunit.assemblies "C:\TeamCity\buildAgent\work\884196217c13b442\bin\Debug\SeleniumExample.dll"
 end
